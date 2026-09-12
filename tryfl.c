b819fccddc0989b21b55953ffcee77f0ef2d899b
Steve Kirkendall <kirkenda@gmail.com>
2026-09-12 10:09:32 -0700

    Trimmed out some debugging code and superfluous #includes, to let it serve as a simple example for the use of fineline.

--- tryfl.c
+++ tryfl.c
@@ -1,19 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
-#include <unistd.h>
 #include "fineline.h"
 
 int main(int argc, char **argv)
 {
-	char *prompt, *line;
+	char *line;
 
 	setlocale(LC_ALL, "");
-	//prompt = "Try\xe2\x96\xb6";
-	prompt = "Try>";
-	fprintf(stderr, "pid: %d    promptwidth: %d   prompt=\"%s\"\r\n", (int)getpid(), fineline_char_column_number(prompt, strlen(prompt), 8), prompt);
-	while ((line = fineline(prompt)) != NULL) {
+	while ((line = fineline("Try>")) != NULL) {
 		printf("\"%s\"\n", line);
 		free(line);
 	}
