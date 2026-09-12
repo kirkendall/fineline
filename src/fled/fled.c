65c3b038e10a3c37e3e35873c22f8790d96e188f
Steve Kirkendall <kirkenda@gmail.com>
2026-09-12 10:09:11 -0700

    First working version

--- src/fled/fled.c
+++ src/fled/fled.c
@@ -1,21 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <wchar.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <fineline.h>
 
 int main(int argc, char **argv)
 {
-	char *result;
-	fineline_t *editor;
+	char *textin, *textout;
+	size_t textlen, textsize;
+	ssize_t more;
+	fineline_t *fine, *editor;
+	struct stat st;
+	int	fd;
+
+	/* Common flags */
+	if (argc > 2 || (argc > 1 && !strcmp(argv[1], "--help"))) {
+		puts("Usage: fled [filename]");
+		return 0;
+	}
+	if (!argc > 1 && !strcmp(argv[1], "--version")) {
+		printf("fled %s copyright 2027 by Steve Kirkendall\n", FINELINE_VERSION);
+		puts("Freely redistributable under the terms of the GNU Limited General Public License");
+		return 0;
+	}
+
+	/* Expect one filename */
+	if (argc < 2 || stat(argv[1], &st) < 0 || st.st_size == 0) {
+		textsize = 1;
+		textlen = 1;
+		textin = strdup("");
+	} else {
+		textsize = st.st_size;
+		textin = malloc(textsize);
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0) {
+			perror(argv[1]);
+			return 1;
+		}
+		for (textlen = 0; textlen < textsize && (more = read(fd, textin+textlen, textsize - textlen)) > 0; textlen += more) {
+		}
+		close(fd);
+
+		/* trim the last newline */
+		if (textlen > 0 && textin[textlen - 1] == '\n')
+			textin[--textlen] = '\0';
+	}
 
 	/* Create an edit buffer */
-	editor = fineline_tty_alloc();
-
-	/* Load a file into the edit buffer */
+	fine = fineline_tty_alloc();
+	editor = fineline_mode_editor(fine, textin, textlen);
 
 	/* Edit it */
-	result = fineline_tty(editor, "  1>");
+	textout = fineline_tty(editor, "  1|");
 
 	/* Maybe save it */
+	if (strcmp(textin, textout)) {
+		fd = open(argv[1], O_WRONLY);
+		if (fd < 0) {
+			perror(argv[1]);
+			return 1;
+		}
+		write(fd, textout, strlen(textout));
+		write(fd, "\n", 1);
+		close(fd);
+	}
+
+	/* Clean up */
+	free(textin);
+	free(textout);
+	fineline_free(editor);
+	fineline_free(fine);
 
 	return 0;
 }
